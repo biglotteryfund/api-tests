318476dfd216536bfb81c74bdd0e6459e2e68629
David Rapson <david.rapson@biglotteryfund.org.uk>
2018-03-16 15:53:43 +0000

    Skip hero image test until endpoint is live

--- test-suites/hero-image.test.js
+++ test-suites/hero-image.test.js
@@ -1,7 +1,7 @@
 const superagent = require('superagent');
 const { apiUrl, expectCommonResponse, expectHeroShape } = require('./helpers');
 
-test('it should return hero image for a given slug', () => {
+test.skip('it should return hero image for a given slug', () => {
   return superagent
     .get(apiUrl('/v1/en/hero-image/passion-4-fusion'))
     .then(res => {
