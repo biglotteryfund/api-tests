19e26fa8090b52503aa15d445d46f19c2a9850df
David Rapson <david.rapson@tnlcommunityfund.org.uk>
2020-04-07 11:52:00 +0100

    Fix homepage test (optional caption)

--- test-suites/homepage.test.js
+++ test-suites/homepage.test.js
@@ -4,15 +4,14 @@ const { apiUrl, expectCommonResponse, expectListShape } = require('./helpers');
 const mediaRegex = /\.jpg/;
 const homepageFeaturedLinkShape = {
   href: expect.any(String),
+  label: expect.any(String),
   image: expect.objectContaining({
     title: expect.any(String),
-    caption: expect.any(String),
     default: expect.stringMatching(mediaRegex),
     small: expect.stringMatching(mediaRegex),
     medium: expect.stringMatching(mediaRegex),
     large: expect.stringMatching(mediaRegex),
-  }),
-  label: expect.any(String),
+  })
 };
 
 test('it should get data for the homepage', () => {
@@ -22,9 +21,6 @@ test('it should get data for the homepage', () => {
       const { body } = res;
       const attrs = body.data.attributes;
       expectCommonResponse({ res });
-      expect(attrs.featuredLinks[0]).toEqual(
-        expect.objectContaining(homepageFeaturedLinkShape)
-      );
       expectListShape(attrs.featuredLinks, homepageFeaturedLinkShape);
     });
 });
